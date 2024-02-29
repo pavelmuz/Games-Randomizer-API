78a673e41f3a725568ee011758e16083a0ab3b19
pavelmuzalevskiy <pavel.muzalevsky@gmail.com>
2024-03-01 00:19:34 +0300

    Создан Dockerfile

--- games_randomizer/settings.py
+++ games_randomizer/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*3z%t-6us&(e)v2cr06!2@wa68t1)e(7z8dxamaj8st-pl55te'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
